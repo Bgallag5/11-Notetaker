05ca45999217f5545cb73eb4c44807fbc0fd0bf1
bengallagher <bgallag5@gmail.com>
2021-07-30 10:59:05 -0400

    app.post writes new json to file

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+const pageLoad = function(){
+    //load .db to notes page on load 
+}
 
 // const createNote = function (body, array){
 //     //pass in new note with req.body and existing note array
-//     //push new note to notes array => fs.WriteFile(notesarray)
+//     //push new note to notes array => fs.WriteFile(stringify(notesarray))
 // }
 
 ////one get route for the homepage
@@ -29,22 +32,23 @@ app.get("/notes", (req, res) => {
 res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
 
-app.get("/api/notes", (req, res) => {
-    let results = notes;
 
-    res.json(results);
+app.get("/api/notes", (req, res) => {
+    console.log(notesArray);
+    return res.json(notesArray);
+    
   });
 
 app.post("/api/notes", (req, res) =>{
     // req.body.id = notes.length.toString();  //// add unique id to each new note
     // const note = createNote(req.body, notes);
     const note = req.body;
-    notesArray.push(note)
-    res.json(note);
+    note.id = (notesArray.length).toString();
+    notesArray.push(note);
+
+    fs.writeFileSync("db/db.json", JSON.stringify(notesArray))
+    res.json(notesArray);
 })
-
-
-
 
 
 app.listen(3001, () => {
